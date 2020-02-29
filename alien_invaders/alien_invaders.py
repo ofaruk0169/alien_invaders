9db4196bfa233512487b564e304faf3e916bd910
Omar Faruk <ofaruk0169@gmail.com>
2020-02-29 20:30:55 +0000

    bullets have been added

--- alien_invaders/alien_invaders.py
+++ alien_invaders/alien_invaders.py
@@ -3,6 +3,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvaders:
     """This class manages the assests and behaviour in the game"""
@@ -18,15 +19,17 @@ class AlienInvaders:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         #Background color
         self.bg_color = (220, 220, 210)
 
-    def run_game(self):
+    def run_game(self):   
         "This is the main loop for the game"
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
             
     def _check_events(self):
@@ -49,6 +52,8 @@ class AlienInvaders:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
 
@@ -57,12 +62,19 @@ class AlienInvaders:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """create new bullet and add to the bullets group"""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
         
         
     def _update_screen(self):
         """Redraw screen during each pass trhough loop"""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()  
 
         #The most recently drawn screen is visible
         pygame.display.flip()
--- alien_invaders/settings.py
+++ alien_invaders/settings.py
@@ -10,3 +10,9 @@ class Settings:
 
         #speed of ship
         self.ship_speed = 2.1
+
+        #Bullets
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
