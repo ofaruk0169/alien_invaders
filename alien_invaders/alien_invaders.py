48f7068ed443a9efe97e3bacf61bb9ac73eef37f
Omar Faruk <ofaruk0169@gmail.com>
2020-02-29 11:56:16 +0000

    fullscreen mode has been added and Q for quit

--- alien_invaders/alien_invaders.py
+++ alien_invaders/alien_invaders.py
@@ -12,8 +12,9 @@ class AlienInvaders:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -46,6 +47,8 @@ class AlienInvaders:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
 
