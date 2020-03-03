213db6ae3b95eb1ce1b3733156e050def0231d08
Omar Faruk <ofaruk0169@gmail.com>
2020-03-03 00:04:15 +0000

    fleet of aliens has been added

--- alien_invaders/alien_invaders.py
+++ alien_invaders/alien_invaders.py
@@ -101,7 +101,34 @@ class AlienInvaders:
         """create a fleet of aliens"""
         #make an alien
         alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        # The available space for teh aliens is calculated as double the width of an alien is taken away from the screen length 
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        #The spacing between aliens. So double alien width divided by the available space.
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        #Number of rows aliens will fit inside
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - 
+            (3 * alien_height) - ship_height)
+        number_rows = available_space_y // (2 * alien_height)
+
+        #create fleet of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+    
+    def _create_alien(self, alien_number, row_number):
+        """create alien in row"""
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+            
+            
 
 
 
